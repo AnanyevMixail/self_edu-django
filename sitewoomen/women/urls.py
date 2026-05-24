ee0cb70421af2c4b05765cd1d34f01f5b558bc3b
Ananyev Mixail <ananyevmixail@gmail.com>
2026-05-24 23:48:48 +0400

    initial commit

---
+++ sitewoomen/women/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, re_path
+from . import views
+
+urlpatterns = [
+    path('', views.index , name='home'),          # главная
+    path('about/', views.about , name='about'),
+    path('addpage/', views.addpage, name='add_page'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    path('post/<int:post_id>/', views.show_post, name='post'),
+     
+]
---
+++ sitewoomen/women/views.py
@@ -0,0 +1,45 @@
+from django.http import Http404, HttpResponse, HttpResponseNotFound
+from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
+
+
+menu = [
+    {'title': "О сайте", 'url_name': 'about' },
+    {'title': "Добавить статью", 'url_name': 'add_page' },
+    {'title': "Обратная связь", 'url_name': 'contact' },
+    {'title': "Войти", 'url_name': 'login' },
+]
+
+
+data_db = [
+    {'id': 1, 'title': 'Анжелина Джоли', 'content': 'Биография Анжелины Джоли', 'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
+]
+
+# Create your views here.
+def index(request):
+    data = {'title': 'Главная страница',
+            'menu' : menu,
+            'posts': data_db,
+            }
+    return render(request, 'women/index.html', context=data)
+
+def about(request):
+    data = {'title': 'О сайте'}
+    return render(request, 'women/about.html', {'title': 'О сайте'})
+
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
+
+def addpage(request):
+    return HttpResponse("Добавление статьи")
+
+def contact(request):
+    return HttpResponse("Обратная Связь")
+
+def login(request):
+    return HttpResponse("Авторизация")
+
+def page_not_found (request, exception):
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
